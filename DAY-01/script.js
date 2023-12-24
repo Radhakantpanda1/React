cb76b8e183d23f11916ad206835de2dade3d7e2a
Radhakant Panda <114462764+Radhakantpanda1@users.noreply.github.com>
2023-12-24 21:58:00 +0530

    day-01 of react

---
+++ DAY-01/script.js
@@ -0,0 +1,11 @@
+
+let hh=React.createElement('h1',{id:"head"},"hello world from react");
+console.log(hh);// object
+const root=ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);// object
+root.render(hh);
+let parent=React.createElement("div",{id:'parent'},[React.createElement("div",{id:'child'},[React.createElement("h1",{id:'hh1'},"I AM H1 USING REACT"),React.createElement("h2",{id:'hh2'},"I AM H2 USING REACT")]),React.createElement("div",{id:'child'},[React.createElement("h1",{id:'hh1'},"I AM H1 USING REACT"),React.createElement("h2",{id:'hh2'},"I AM H2 USING REACT")])]);
+const groot=ReactDOM.createRoot(document.getElementById("groot"));
+groot.render(parent);
+
+
